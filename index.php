a4cb98df88ac37198514068634b8ee32a388fa86
Teja Vardhan Madamanchi <tejavardhan.m@ad.infosys.com>
2023-12-10 21:51:56 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+    <head>
+        <title>HomeNest</title>
+        <link href="style_index.css" rel="stylesheet"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+
+        <div class="full">
+            <div class="part1">
+                <br><center>
+                <div class="part1_inside">
+                    <h1>Ready To Begin <br> a New Journey</h1>
+                    <div class="button1">
+                        <a href="register.php">List A Property</a>
+                    </div>
+                    <div class = "button2">
+                    <a href="register.php">Buy A Property</a>
+                    </div>
+                </div></center>
+            </div>
+            <div class="part2">
+                <div class="part2_inside">
+                    <div class="part2_img">
+                        <img src="img/2.jpeg">
+                    </div>
+                    <div class="part2_inner">
+                        <h1>What We Specialize at </h1>
+                        <hr>
+                        <table>
+                            <tr>
+                                <td><h3>Explore a diverse range of rental options, from residential properties and commercial spaces, and more.</h3></td>
+                                <td><h3>Our intuitive platform ensures a seamless experience, making it easy for you to find the ideal rental with advanced search features.</h3></td>
+
+                            </tr>
+                            <tr>
+                                <td><h3>Trust in accurate information. Our listings go through a thorough verification process to ensure reliability.</h3></td>
+                                <td><h3>We're here for you. Our dedicated support services cater to both tenants and landlords, ensuring a positive experience for everyone.</h3></td>
+
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="part3">
+            <video  autoplay muted loop>
+                <source src="img/video1.mp4" type="video/mp4">
+            </video>
+            <h1>Why Choose HomeNest</h1>
+            <center><table>
+                <tr>
+                    <td>Find your ideal rental, be it an <br>apartment, office, or special equipment..</td>
+                    <td>Navigate effortlessly with our <br>user-friendly platform.</td>
+                    <td>Rigorous verification for <br>accurate, reliable listings.</td>
+                    <td>Personalized assistance <br>from our dedicated team.</td>
+                </tr>
+            </table></center>
+
+            <table>
+                <tr>
+                    <td><h1>GET IN TOUCH</h1></td>
+                    <td>Main Office <br> 509 Lindbergh PN NE,<br> Atlanta, Georgia, 30324</td>
+                    <td>Tel: (470)999-9999 <br> Email: HomeNest@gmail.com <br> Social: @HomeNest</td>
+                    <td>
+                        <a href="https://www.facebook.com/willowestlindbergh" class="fa fa-facebook"></a>
+                        <a href="https://twitter.com/WillowestYT" class="fa fa-twitter"></a>
+                        <a href="https://www.instagram.com/willowestlindbergh/?hl=en" class="fa fa-instagram"></a>
+
+                    </td>
+                </tr>
+            </table>
+                
+            </div>
+        </div>
+
+    </body>
+</html>
